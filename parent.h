2ba1b28a3939c508c5298fd121dd890479f9d6c4
sbwang <sbwang@student.cs.uwaterloo.ca>
2015-12-05 14:37:39 -0500

    Added parent class

---
+++ parent.h
@@ -0,0 +1,15 @@
+#ifndef _PARENT_H_
+#define _PARENT_H_
+
+_Task Parent {
+    private:
+        unsigned int _numStudents;
+        unisgned int _parentalDelay;
+        Printer& _prt;
+        Bank& _bank;
+        void main();
+    public:
+        Parent( Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay );
+}
+
+#endif
