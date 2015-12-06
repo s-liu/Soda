6978272faf99d06161e96b83ca3d9f5b53564f8e
sbwang <sbwang@student.cs.uwaterloo.ca>
2015-12-05 21:48:21 -0500

    Added printer class

--- printer.cc
+++ printer.cc
@@ -20,7 +20,7 @@ Printer::Printer( unsigned int numStudents, unsigned int numVendingMachines, uns
         cout << endl;
     }
 
-int Printer::getIndex(Kind kind) {
+unsigned int Printer::getIndex(Kind kind) {
     switch(kind){
         case Kind::Parent: return 0;
         case Kind::Groupoff: return 1;
@@ -32,7 +32,7 @@ int Printer::getIndex(Kind kind) {
         case Kind::Vending: return 6 + _numStudents;
         case Kind::Courier: return 6 + _numStudents + _numVendingMachines;
     }
-    return -1;
+    return 404; // Should not reach here
 }
 
 void Printer::printAll() {
@@ -42,28 +42,71 @@ void Printer::printAll() {
             cout << "\t";
             continue;
         }
-
         if(info -> kind == Kind::Parent) {
-            if(info -> state == 'S') {
-                cout << 'S';
+            if(info -> state == 'S' || info -> state == 'F') {
+                cout << info -> state;
             } else if (info -> state == 'D') {
-                cout << 'D' << info -> lid << info -> value1;
-            } else if (info -> state == 'F') {
-                cout << 'F';
+                cout << info -> state << info -> lid << info -> value1;
             }
         } else if(info -> kind == Kind::Groupoff) {
-            
-            
+            if(info -> state == 'S' || info -> state == 'F') {
+                cout << info -> state;
+            } else if (info -> state == 'D') {
+                cout << info -> state << info -> value1;
+            }
         } else if(info -> kind == Kind::WATCardOffice) {
-        
+            if(info -> state == 'S' || info -> state == 'W' || info -> state == 'F') {
+                cout << info -> state;
+            } else if (info -> state == 'C' || info -> state == 'T') {
+                cout << info -> state << info -> lid << info -> value1;
+            }
         } else if(info -> kind == Kind::NameServer) {
-        
+            if(info -> state == 'S' || info -> state == 'F') {
+                cout << info -> state;
+            } else if (info -> state == 'N') {
+                cout << info -> state << info -> lid << info -> value1;
+            } else if (info -> state == 'R') {
+                cout << info -> state << info -> lid;
+            }
         } else if(info -> kind == Kind::Truck) {
-        
+            if(info -> state == 'S' || info -> state == 'F') {
+                 cout << info -> state;
+            } else if (info -> state == 'd' || info -> state == 'U' || info -> state == 'D') {
+                cout << info -> state << info -> lid << info -> value1;
+            } else if (info -> state == 'P') {
+                cout << info -> state << info -> value1;
+            }
+        } else if(info -> kind == Kind::BottlingPlant) {
+            if(info -> state == 'S' || info -> state == 'F' || info -> state == 'P') {
+                cout << info -> state;
+            } else if (info -> state == 'G') {
+                cout << info -> state << info -> value1;
+            }
+        } else if(info -> kind == Kind::Student) {
+            if(info -> state == 'L' || info -> state == 'F') {
+                cout << info -> state;
+            } else if (info -> state == 'V') {
+                cout << info -> state << info -> lid;
+            } else if (info -> state == 'S') {
+                cout << info -> state << info -> value1 << info -> value2 ;
+            } 
+        } else if(info -> kind == Kind::Vending) {
+            if(info -> state == 'r' || info -> state == 'F' || info -> state == 'R') {
+                cout << info -> state;
+            } else if (info -> state == 'B') {
+                cout << info -> state << info -> value1 << info -> value2;
+            } else if (info -> state == 'S') {
+                cout << info -> state << info -> value1;
+            }
+        } else if(info -> kind == Kind::Courier) {
+            if(info -> state == 'S' || info -> state == 'F') {
+                cout << info -> state;
+            } else if (info -> state == 't' || info -> state == 'T') {
+                cout << info -> state << info -> lid << info -> value1;
+            }
         }
 
         cout << "\t";
-
         // Delete the info object
         // And set the index to null
         delete info;
@@ -71,26 +114,123 @@ void Printer::printAll() {
     }
 }
 
-void Printer::print( Kind kind, char state ){
+void Printer::printFinished(unsigned int index) {
+    printAll();
+    for(unsigned int i = 0; i < _infos.size(); i++) {
+        if(i == index) {
+            cout << 'F';
+        } else {
+            cout << "...";
+        }
+        cout << "\t";
+    }
+    cout << endl;
+}
 
+void Printer::print( Kind kind, char state ){
+    unsigned int index = getIndex(kind);
+    if(state == 'F') {
+        printFinished(index);
+        return;
+    }
+
+    if(_infos[index] != NULL) {
+        printAll();
+    }
+
+    Info* info = new Info();
+    info -> kind = kind;
+    info -> state = state;
+    _infos[index] = info;
 }
 
 void Printer::print( Kind kind, char state, int value1 ){
+    unsigned int index = getIndex(kind);
+    if(state == 'F') {
+        printFinished(index);
+        return;
+    }
 
+    if(_infos[index] != NULL) {
+        printAll();
+    }
+
+    Info* info = new Info();
+    info -> kind = kind;
+    info -> state = state;
+    info -> value1 = value1;
+    _infos[index] = info;
 }
 
 void Printer::print( Kind kind, char state, int value1, int value2 ){
+    unsigned int index = getIndex(kind);
+    if(state == 'F') {
+        printFinished(index);
+        return;
+    }
 
+    if(_infos[index] != NULL) {
+        printAll();
+    }
+
+    Info* info = new Info();
+    info -> kind = kind;
+    info -> state = state;
+    info -> value1 = value1;
+    info -> value2 = value2;
+    _infos[index] = info;
 }
 
 void Printer::print( Kind kind, unsigned int lid, char state ){
+    unsigned int index = getIndex(kind) + lid;
+    if(state == 'F') {
+        printFinished(index);
+        return;
+    }
 
+    if(_infos[index] != NULL) {
+        printAll();
+    }
+
+    Info* info = new Info();
+    info -> kind = kind;
+    info -> state = state;
+    _infos[index] = info;
 }
 
 void Printer::print( Kind kind, unsigned int lid, char state, int value1 ){
+    unsigned int index = getIndex(kind) + lid;
+    if(state == 'F') {
+        printFinished(index);
+        return;
+    }
 
+    if(_infos[index] != NULL) {
+        printAll();
+    }
+
+    Info* info = new Info();
+    info -> kind = kind;
+    info -> state = state;
+    info -> value1 = value1;
+    _infos[index] = info;
 }
 
 void Printer::print( Kind kind, unsigned int lid, char state, int value1, int value2 ){
+    unsigned int index = getIndex(kind) + lid;
+    if(state == 'F') {
+        printFinished(index);
+        return;
+    }
 
+    if(_infos[index] != NULL) {
+        printAll();
+    }
+
+    Info* info = new Info();
+    info -> kind = kind;
+    info -> state = state;
+    info -> value1 = value1;
+    info -> value2 = value2;
+    _infos[index] = info;
 }
---
+++ printer.h
@@ -0,0 +1,40 @@
+#ifndef _PRINTER_H_
+#define _PRINTER_H_
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+_Monitor Printer {
+    private:
+        unsigned int _numStudents;
+        unsigned int _numVendingMachines;
+        unsigned int _numCouriers;
+    public:
+        enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
+        Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+        void print( Kind kind, char state );
+        void print( Kind kind, char state, int value1 );
+        void print( Kind kind, char state, int value1, int value2 );
+        void print( Kind kind, unsigned int lid, char state );
+        void print( Kind kind, unsigned int lid, char state, int value1 );
+        void print( Kind kind, unsigned int lid, char state, int value1, int value2 );
+
+    private:
+        typedef struct {
+            Kind kind;
+            unsigned int lid;
+            char state;
+            int value1;
+            int value2;
+        } Info;
+        vector<Info*> _infos;
+        void printAll();
+        unsigned int getIndex(Kind kind);
+        void printFinished(unsigned int index);
+};
+
+#endif
